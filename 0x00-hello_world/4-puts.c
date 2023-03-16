9f471f809e58cf6abddd305621b7939e2ef572c1
Sonia Nwagbogun <soniachinwe176@gmail.com>
2023-03-16 11:30:59 -0700

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <sttdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Retuen: 0 if exited properly, non-zero otherwise
+ * /
+ int main (void)
+ {
+           puts("\"Programming is like building a multilingual puzzle");
+	   return (0);
+ }
