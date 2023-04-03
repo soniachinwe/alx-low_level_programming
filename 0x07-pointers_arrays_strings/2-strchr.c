2ed6b50b126060a41fb2e1d6dd53062993192c6c
Sonia Nwagbogun <soniachinwe176@gmail.com>
2023-04-03 05:34:18 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * *_strchr - locates a character in a string
  * @s: string to search
  * @c: char to find
- * @char: data type
+ * 
  * Return: a pointer to the first occurrence of the character
  * c in the string s, or NULL if the character is not found
  */
