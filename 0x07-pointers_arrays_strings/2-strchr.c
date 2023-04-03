ef6f0cf0f2210736240e147779331a6c3b1eba40
Sonia Nwagbogun <soniachinwe176@gmail.com>
2023-04-03 05:23:47 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * *_memcpy - copies memory area
- * @dest: destination memory area
- * @src: memory area to copy from
- * @n: number of bytes to copy
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
  * @char: data type
- * Return: pointer to dest
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_strchr(char *s, char c)
 {
-	unsigned int i;
+		int j;
 
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	return (dest);
+		while (1)
+		{
+			j = *s++;
+			if (j == c)
+			{
+				return (s - 1);
+			}
+			if (j == 0)
+			{
+				return (NULL);
+			}
+		}
 }
