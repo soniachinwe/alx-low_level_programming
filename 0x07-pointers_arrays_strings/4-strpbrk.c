6e87d4809cfef876cac3b7648ec4aa8797259b19
Sonia Nwagbogun <soniachinwe176@gmail.com>
2023-04-04 11:12:39 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,8 +3,8 @@
 
 /**
  * *_strpbrk : searches a string for any of a set of bytes
- * s: string to search
- * accept: stringcontaining the bytes to look for
+ * @s: string to search
+ * @accept: stringcontaining the bytes to look for
  *
  * Return: pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found
